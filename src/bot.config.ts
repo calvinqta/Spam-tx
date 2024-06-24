c64e55ac782b854abf08aaf764f1d4c02ca9e168
calvinqta <130379390+calvinqta@users.noreply.github.com>
2024-06-24 12:04:59 +0700

    Update bot.config.ts

--- src/bot.config.ts
+++ src/bot.config.ts
@@ -18,28 +18,22 @@ export interface BotConfig {
   orchestratorMinFunds: string;
   serverPort: number;
 }
-
- export async function getConfig(): Promise<BotConfig> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        rpcUrl: stringFromEnvOrThrow('RPC_URL'),
-        orchestratorAccountPrivateKey: stringFromEnvOrThrow('ORCH_PRIV_KEY'),
-        orchestratorMinFunds: stringFromEnvOrDefault(
-          'ORCH_MIN_FUNDS_BASE',
-          '10000000000000000000'
-        ),
-        numberOfAccounts: numberFromEnvOrDefault('NUMBER_OF_ACCOUNTS', 1000000),
-        fundsPerAccount: stringFromEnvOrDefault(
-          'FUNDS_PER_ACCOUNT_BASE',
-          '1000000000000000000'
-        ),
-        waitForTxToMine: booleanFromEnvOrDefault('WAIT_FOR_TX_MINE', false),
-        gasToConsumePerTx: stringFromEnvOrDefault('GAS_CONSUME_PER_TX', '100000'),
-        logLevel: logLevelFromEnvOrDefault('LOG_LEVEL', 'info'),
-        serverPort: numberFromEnvOrDefault('SERVER_PORT', 8080),
-      });
-    }, 1000); // 1000 milliseconds = 1 second
-  });
+export function getConfig(): BotConfig {
+  return {
+    rpcUrl: stringFromEnvOrThrow('RPC_URL'),
+    orchestratorAccountPrivateKey: stringFromEnvOrThrow('ORCH_PRIV_KEY'),
+    orchestratorMinFunds: stringFromEnvOrDefault(
+      'ORCH_MIN_FUNDS_BASE',
+      '10000000000000000000'
+    ),
+    numberOfAccounts: numberFromEnvOrDefault('NUMBER_OF_ACCOUNTS', 10),
+    fundsPerAccount: stringFromEnvOrDefault(
+      'FUNDS_PER_ACCOUNT_BASE',
+      '1000000000000000000'
+    ),
+    waitForTxToMine: booleanFromEnvOrDefault('WAIT_FOR_TX_MINE', false),
+    gasToConsumePerTx: stringFromEnvOrDefault('GAS_CONSUME_PER_TX', '100000'),
+    logLevel: logLevelFromEnvOrDefault('LOG_LEVEL', 'info'),
+    serverPort: numberFromEnvOrDefault('SERVER_PORT', 8080)
+  };
 }
-
